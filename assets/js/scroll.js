c1bd715d0b0246dc3f2952a05cde667ef30aba36
Minecraft一角钱 <122196845+MinecraftYJQ@users.noreply.github.com>
2024-10-20 12:44:34 +0800

    Add files via upload

---
+++ assets/js/scroll.js
@@ -0,0 +1,21 @@
+// scrollListener.js
+window.addEventListener('scroll', function() {
+    var scrollY = window.scrollY;
+    var topbar = document.querySelector('.topbar');
+  
+    if (scrollY > 50 && topbar) {
+        topbar.style.top = '';
+        topbar.style.right = '';
+        topbar.style.left = '';
+        topbar.style.borderRadius  = '10px';
+        topbar.style.backgroundColor  = "rgba(0, 0, 0, 0.3)";
+        topbar.style.boxShadow  = "0 0 10px #b8b8b8";
+    } else if (scrollY <= 50 && topbar) {
+        topbar.style.top = '0';
+        topbar.style.right = '0';
+        topbar.style.left = '0';
+        topbar.style.backgroundColor  = "rgba(0, 0, 0, 0.3)";
+        topbar.style.boxShadow  = "0 0 0 #b8b8b8";
+        topbar.style.borderRadius  = '0px';
+    }
+  });
